922bab03e49ea7b3f1dfca9f432b4ca28107feea
buskun <bus_sk136@hotmail.co.th>
2016-08-03 22:23:35 +0700

    add second pg

--- resources/views/Lol Com - Bus/script.js
+++ resources/views/Lol Com - Bus/script.js
@@ -30,12 +30,24 @@ $("form > div.page").each(function(index, value) {
 	});
 });
 
+$("#Pg > div.section").each(function(index, value) {
+	$(value).css({
+		"top": (index * 100) + "%"
+	});
+});
+
 $("#page-final").css({
 	"left": ($("div.page").length * 100) + "%"
 })
 
 $(".page").find("input[required]").each(function(index, value) {
 	$(value).prev("label").append("<span class='req'>*</span>");
+});
+
+$("#moreInfoBtn").on("click", function() {
+	$("#Pg").animate({
+		"top": "-100%"
+	}, 1000);
 });
 
 $(window).on("click", function() {
