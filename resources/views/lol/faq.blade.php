55e4b4515f5fa9c4eae90c7c5e54df89b60277d3
Pittawat2542 <pittawat2542@hotmail.com>
2016-08-06 22:14:18 +0700

    Change Description

--- resources/views/lol/faq.blade.php
+++ resources/views/lol/faq.blade.php
@@ -5,13 +5,13 @@
             <div class="row">
                 <div class="col-md-6 right">
                     <h2 class="subtitle text-center">สถานที่จัดการแข่งขัน</h2>
-                        <h3 class="supersubtitle">รอบคัดเลือก</h3>
+                        <h3 class="supersubtitle">รอบคัดเลือก และ Semi-Final</h3>
                         <p class="description2">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit voluptates eos quas deleniti similique, magni omnis a impedit itaque sed tempora aliquid quos laborum error, fuga quis velit aspernatur dicta!
+                            แข่งขันออนไลน์ เพื่อหาผู้เข้าแข่งขันในรอบ และ Final
                         </p>
-                        <h3 class="supersubtitle">รอบชิงชนะเลิศ</h3>
+                        <h3 class="supersubtitle">รอบ Final</h3>
                         <p class="description2">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit voluptates eos quas deleniti similique, magni omnis a impedit itaque sed tempora aliquid quos laborum error, fuga quis velit aspernatur dicta!
+                            แข่งขันที่อาคารสวนกุหลาบรำลึก ชั้น 3 โรงเรียนสวนกุหลาบวิทยาลัย
                         </p>
                 </div>
             </div>
