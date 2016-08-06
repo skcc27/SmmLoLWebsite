d842495b00268f7b1d653035431169bce49f8c55
oatts <thanatoat@hotmail.com>
2016-08-06 12:55:17 +0700

    change text

--- resources/views/lol/info.blade.php
+++ resources/views/lol/info.blade.php
@@ -5,15 +5,10 @@
             <div class="row">
                 <div class="col-md-6 right">
                     <p class="description">
-                        &emsp;&emsp;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt quisquam pariatur
-                        odio aliquid quas ab quasi iure, provident doloremque odit velit voluptatem qui adipisci
-                        delectus optio eligendi, magnam eos voluptas? Quam, est! Tenetur
-                        rerum, labore ipsa velit, cum natus fuga aperiam expedita eaque repudiandae magnam nobis, cumque
-                        ducimus laboriosam, sapiente sequi enim soluta impedit officia ipsam tempora. Similique, ullam,
-                        temporibus? Aperiam qui, quis
-                        assumenda tenetur. Neque soluta vitae earum explicabo amet dolores suscipit culpa omnis
-                        voluptates, nam recusandae, sunt aliquid at fugiat expedita! Laboriosam, doloribus praesentium
-                        repudiandae asperiores cupiditate consequatur?
+                        &emsp;&emsp;การแข่งขัน Samarnmitr League of Legends Competition ได้จัดขึ้นเพื่อเฟ้นหาทีมนักเรียน</br>
+                        โรงเรียนสวนกุหลาบวิทยาลัย ที่มีความสามารถในการเล่นเกม League of Legends จัดขึ้นโดย</br>
+                        ชุมนุมคอมพิวเตอร์ โรงเรียนสวนกุหลาบวิทยาลัย ภายใต้การสนับสนุนของ Garena ผู้ให้บริการเกม League of Legends</br> เพื่อชิงเงินรางวัลมูลค่ารวมกว่า 6,000 บาท!!! เพื่อส่งเสริมการเล่นเกมอย่างสร้างสรรค์ และเกิดประโยชน์</br> โดยเปิดรับสมัครตั้งแต่วันนี้ ถึง วันอาทิตย์ที่ 21 สิงหาคม พ.ศ. 2559 เวลา 22.00 น.</br>
+                        โดยเริ่มต้นการแข่งขันในรอบแรก ในวันจันทร์ที่ 22 สิงหาคม พ.ศ. 2559 ถึง วันอาทิตย์ที่ 4 กันยายน พ.ศ.2559</br> และแข่งขันรอบรองชนะเลิศ อันดับ 2 และ รอบชิงชนะเลิศในวันสมานมิตรที่จะถึงนี้ นักเรียนสวนกุหลาบวิทยาลัยที่สนใจ</br> สามารถเข้าร่วมการแข่งขันได้ในรูปแบบทีม 5-6 คน ผ่านทางเว็บไซต์ smmlol.tk</br>
                     </p>
                 </div>
             </div>
