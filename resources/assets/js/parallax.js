7e0c5358b19a63ff9df7e4125ec1594e6516f741
MoKunz <pavat.tangmo@hotmail.com>
2016-08-06 19:15:03 +0700

    Bugfix for moreinfo button

--- resources/assets/js/parallax.js
+++ resources/assets/js/parallax.js
@@ -9,44 +9,47 @@ var totalSlideNumber = $(".background").length;
 
 // ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
 function parallaxScroll(evt) {
-  if (isFirefox) {
-    //Set delta for Firefox
-    delta = evt.detail * (-120);
-  } else if (isIe) {
-    //Set delta for IE
-    delta = -evt.deltaY;
-  } else {
-    //Set delta for all other browsers
-    delta = evt.wheelDelta;
-  }
+    var delta = 0;
+    if (evt == null) {
+        delta = -120;
+    } else if (isFirefox) {
+        //Set delta for Firefox
+        delta = evt.detail * (-120);
+    } else if (isIe) {
+        //Set delta for IE
+        delta = -evt.deltaY;
+    } else {
+        //Set delta for all other browsers
+        delta = evt.wheelDelta;
+    }
 
-  if (ticking != true) {
-    if (delta <= -scrollSensitivitySetting) {
-      //Down scroll
-      ticking = true;
-      if (currentSlideNumber !== totalSlideNumber - 1) {
-        currentSlideNumber++;
-        nextItem();
-      }
-      slideDurationTimeout(slideDurationSetting);
+    if (ticking != true) {
+        if (delta <= -scrollSensitivitySetting) {
+            //Down scroll
+            ticking = true;
+            if (currentSlideNumber !== totalSlideNumber - 1) {
+                currentSlideNumber++;
+                nextItem();
+            }
+            slideDurationTimeout(slideDurationSetting);
+        }
+        if (delta >= scrollSensitivitySetting) {
+            //Up scroll
+            ticking = true;
+            if (currentSlideNumber !== 0) {
+                currentSlideNumber--;
+            }
+            previousItem();
+            slideDurationTimeout(slideDurationSetting);
+        }
     }
-    if (delta >= scrollSensitivitySetting) {
-      //Up scroll
-      ticking = true;
-      if (currentSlideNumber !== 0) {
-        currentSlideNumber--;
-      }
-      previousItem();
-      slideDurationTimeout(slideDurationSetting);
-    }
-  }
 }
 
 // ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
 function slideDurationTimeout(slideDuration) {
-  setTimeout(function() {
-    ticking = false;
-  }, slideDuration);
+    setTimeout(function () {
+        ticking = false;
+    }, slideDuration);
 }
 
 // ------------- ADD EVENT LISTENER ------------- //
@@ -55,11 +58,11 @@ window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
 
 // ------------- SLIDE MOTION ------------- //
 function nextItem() {
-  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
-  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
+    var $previousSlide = $(".background").eq(currentSlideNumber - 1);
+    $previousSlide.removeClass("up-scroll").addClass("down-scroll");
 }
 
 function previousItem() {
-  var $currentSlide = $(".background").eq(currentSlideNumber);
-  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+    var $currentSlide = $(".background").eq(currentSlideNumber);
+    $currentSlide.removeClass("down-scroll").addClass("up-scroll");
 }
