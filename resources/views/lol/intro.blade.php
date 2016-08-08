b63b68c38a1bee86bee4560893ebb93bbed03c0a
MoKunz <pavat.tangmo@hotmail.com>
2016-08-08 16:22:11 +0700

    Another background and sponsor logo fix (again)

--- resources/views/lol/intro.blade.php
+++ resources/views/lol/intro.blade.php
@@ -45,21 +45,25 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-3 col-xs-6">
-                    <img class="img-responsive" src="/img/sponsor1.png" alt="Garena">
+                <div class="col-md-6 col-md-offset-1 col-sm-10 col-xs-12">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-3 col-xs-6">
+                            <img class="img-responsive" src="/img/sponsor1.png" alt="Garena">
 
-                </div>
-                <div class="col-md-3 col-xs-6">
-                    <img class="img-responsive" src="/img/sponsor2.png" alt="Suabkularb Wittayalai School">
+                        </div>
+                        <div class="col-md-3 col-sm-3 col-xs-6">
+                            <img class="img-responsive" src="/img/sponsor2.png" alt="Suabkularb Wittayalai School">
 
-                </div>
-                <div class="col-md-3 col-xs-6">
-                    <img class="img-responsive" src="/img/sponsor3.png" alt="SK Com Club">
+                        </div>
+                        <div class="col-md-3 col-sm-3 col-xs-6">
+                            <img class="img-responsive" src="/img/sponsor3.png" alt="SK Com Club">
 
-                </div>
-                <div class="col-md-3 col-xs-6">
-                    <img class="img-responsive" src="/img/sponsor4.png" alt="Samarnmitr">
+                        </div>
+                        <div class="col-md-3 col-sm-3 col-xs-6">
+                            <img class="img-responsive" src="/img/sponsor4.png" alt="Samarnmitr">
 
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
